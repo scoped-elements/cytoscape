3a3fadca5aaf724d05bad36e98272ed10535b02d
Guillem Cordoba <guillem.cordoba@gmail.com>
2021-12-07 12:45:13 +0100

    Added cose-bilkent

---
+++ src/cytoscape-cose-bilkent.ts
@@ -0,0 +1,20 @@
+import { property } from 'lit/decorators.js';
+import cytoscape from 'cytoscape';
+
+// @ts-ignore
+import coseBilkent from 'cytoscape-cose-bilkent';
+import { CytoscapeBase } from './cytoscape-base';
+
+cytoscape.use(coseBilkent);
+
+export class CytoscapeCoseBilkent extends CytoscapeBase {
+  @property()
+  coseBilkentOptions = {};
+
+  layout() {
+    return {
+      name: 'cose-bilkent',
+      ...this.coseBilkentOptions,
+    };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './cytoscape-dagre';
 export * from './cytoscape-circle';
 export * from './cytoscape-cola';
+export * from './cytoscape-cose-bilkent';
